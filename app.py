63eae5990e7b8213e11f5c86424367d464c1d2bd
Simbhakk <127213964+Simbhakk@users.noreply.github.com>
2024-03-17 10:11:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
    return 'GreyMatters_Bot'
 
 #Ex https://Greymattersbot:ghp_147bkkabcdefgh@github.com/Greymattersbot/Mogenius
-os.system("git clone https://github.com/Simbhakk/namerokpublic okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 -m main &")
+os.system("git clone https://github.com/Simbhakk/lolpok okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 -m main &")
