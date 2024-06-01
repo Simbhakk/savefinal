8c5db8c462b9dec6d8d0ffb1e180a4b769f207d9
Simbhakk <127213964+Simbhakk@users.noreply.github.com>
2024-06-01 11:04:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-   return 'GreyMatters_Bot'
+   return """
+<center> 
+    <h1>Welcome to Save restricted website </h1>
+    <p> Hi , If you are able to see this website ,</br> then it means our bot is working perfectly</p>
+    <img src="https://graph.org/file/121de3eeefcb047facd3e.jpg" style="border-radius: 12px;"/> 
+</center> 
+<style>
+    body { 
+        background: antiquewhite;
+    }
+</style>"""
 
 #Ex https://Greymattersbot:ghp_147bkkabcdefgh@github.com/Greymattersbot/Mogenius
 os.system("git clone https://github.com/Simbhakk/namakpublic okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 -m main &")
